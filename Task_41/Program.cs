fd508e7214d8f99215fffbb77b7ed5a329d1ae08
RomanGorkavenko <roman@gorkavenko.ru>
2022-05-28 15:29:09 +0300

    modified: README.md new file: Task_41/Program.cs

---
+++ Task_41/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.Write("Введите целые числа через пробел: ");
+string[] arrayM = Console.ReadLine().Split().ToArray();
+
+int[] resultM = new int[arrayM.Length];
+
+int GetCountNumbers(int[] num)
+{
+    int length = num.Length;
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (num[i] > 0) count++;
+    }
+    return count;
+}
+
+
+void PrintArray(int[] arr)
+{
+    string str = " ";
+    foreach (int i in arr)
+    {
+        str += $", {i}";
+    }
+    str = str.Remove(1, 2);
+    str += " ";
+    Console.Write($"{str}");
+}
+
+
+void ExceptionHandling(string[] numbers)
+{
+    int length = numbers.Length;
+    bool yesInt;
+    for (int i = 0; i < length; i++)
+    {
+        yesInt = int.TryParse(numbers[i], out int result);
+        if (yesInt)
+        {
+            resultM[i] = result;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("{0}", numbers[i]);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" -> Некорректный ввод!");
+        }
+    }
+}
+
+ExceptionHandling(arrayM);
+int countM = GetCountNumbers(resultM);
+string str = "числа";
+if (countM > 4) str = "чисел";
+else if (countM == 1) str = "число";
+PrintArray(resultM);
+Console.Write($"-> {countM} {str} больше нуля");
